f6dd15f317eaa310f6ea5e99ea6388becbb0491d
squareCaaat <danny79052@gmail.com>
2024-10-02 02:50:23 +0900

    Add: order Domain and Product class

---
+++ src/order/Product.java
@@ -0,0 +1,13 @@
+package order;
+
+public class Product {
+    private long id;
+    private String name;
+    private int price;
+
+    public Product(long id, String name, int price) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+    }
+}
