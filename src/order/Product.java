7d566f49628aeae618eb2f048c126787a13ff79c
squareCaaat <danny79052@gmail.com>
2024-10-02 23:04:44 +0900

    feat: add toString method and fix type of fields

--- src/order/Product.java
+++ src/order/Product.java
@@ -7,15 +7,19 @@ public class Product {
     private String name;
     private Category category;
     private String brand;
-    private double price;
-    private final int stock;
+    private int price;
+    private int stock;
 
-    public Product(long id, String name, Category category, String brand, double price, int stock) {
+    public Product(long id, String name, Category category, String brand, int price, int stock) {
         this.id = id;
         this.name = name;
         this.category = category;
         this.brand = brand;
         this.price = price;
         this.stock = stock;
+    }
+
+    public String toString(){
+        return "id: " + id + ", name: " + name + ", category: " + category + ", brand: " + brand + ", price: " + price + ", stock: " + stock;
     }
 }
