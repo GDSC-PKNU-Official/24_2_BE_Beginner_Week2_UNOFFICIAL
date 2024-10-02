d4c4949145cfa39ea93cd91369b42d419455ec31
squareCaaat <danny79052@gmail.com>
2024-10-03 00:05:25 +0900

    add: CreditPay Strategy

---
+++ src/payment/CreditPay.java
@@ -0,0 +1,23 @@
+package payment;
+
+import java.time.LocalDateTime;
+
+public class CreditPay implements PaymentStrategy {
+    private static int accumulatedAmount = 0;
+    private LocalDateTime paymentDate = LocalDateTime.now();
+
+    @Override
+    public void pay(int amount) {
+        accumulatedAmount += amount;
+
+        // if (shouldPay())
+        // Customer의 account 감소
+
+        // Customer의 account가 충분치 않다면 account까지 모두
+        // 지불 후 남은 금액은 다음으로 이월 (+ 이자)
+    }
+
+    private boolean shouldPay() {
+        return paymentDate.isEqual(LocalDateTime.now());
+    }
+}
