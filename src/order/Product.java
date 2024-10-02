91247eee4ec6c5d0ad303aa5a074ad5fcd332eee
squareCaaat <140076739+squareCaaat@users.noreply.github.com>
2024-10-02 19:00:11 +0900

    Add: Product Class brand field

--- src/order/Product.java
+++ src/order/Product.java
@@ -3,11 +3,13 @@ package order;
 public class Product {
     private long id;
     private String name;
+    private String brand;
     private int price;
 
-    public Product(long id, String name, int price) {
+    public Product(long id, String name, String brand, int price) {
         this.id = id;
         this.name = name;
+        this.brand = brand;
         this.price = price;
     }
 }
