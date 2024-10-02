7c8d3b52012765e493c1f4c1d66ad1cacc33a5ff
squareCaaat <140076739+squareCaaat@users.noreply.github.com>
2024-10-02 21:04:39 +0900

    feat: Add Product fields

--- src/order/Product.java
+++ src/order/Product.java
@@ -1,15 +1,21 @@
 package order;
 
+import common.Category;
+
 public class Product {
     private long id;
     private String name;
+    private Category category;
     private String brand;
-    private int price;
+    private double price;
+    private final int stock;
 
-    public Product(long id, String name, String brand, int price) {
+    public Product(long id, String name, Category category, String brand, double price, int stock) {
         this.id = id;
         this.name = name;
+        this.category = category;
         this.brand = brand;
         this.price = price;
+        this.stock = stock;
     }
 }
