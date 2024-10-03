b5bf22b2a25755f5560c159290fc6fea9974b3d9
squareCaaat <danny79052@gmail.com>
2024-10-04 01:40:35 +0900

    feat: showSheet

--- src/cart/OrderSheet.java
+++ src/cart/OrderSheet.java
@@ -6,15 +6,17 @@ import java.util.HashMap;
  * 고객이 성공적으로 주문했을 때 고객이 가지고 있는 주문서입니다.
  */
 public class OrderSheet {
-    private long totalAmount;
-    private HashMap<String, Integer> orderedProductHashMap;
+    private final long totalAmount;
+    private final HashMap<String, Integer> orderedProductHashMap;
 
     public OrderSheet(HashMap<String, Integer> orderedProductHashMap, long totalAmount) {
         this.orderedProductHashMap = orderedProductHashMap;
         this.totalAmount = totalAmount;
     }
 
+    // 우선 Cart 클래스의 cart 정보와 total 값을 출력하도록 했습니다.
     public void showSheet(){
-        // Product 매핑 후 필요한 정보만 보여줌
+        orderedProductHashMap.forEach((key, value) -> System.out.println(key + ": " + value));
+        System.out.println("Total Amount: " + totalAmount);
     }
 }
